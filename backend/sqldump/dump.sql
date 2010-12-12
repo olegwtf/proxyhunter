46f349db1702cb9133e25c62e62ac38f88f26132
oleg <oleg@b7df0aa8-d4e2-4c31-b7a9-8b2298e1b5a8>
2010-12-12 07:30:09 +0000

    [FIX] tinyint optimization
    git-svn-id: http://xp-dev.com/svn/proxyhunter@12 b7df0aa8-d4e2-4c31-b7a9-8b2298e1b5a8

--- backend/sqldump/dump.sql
+++ backend/sqldump/dump.sql
@@ -2,14 +2,14 @@ CREATE TABLE  `proxylist` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `host` varchar(15) NOT NULL,
   `port` smallint(5) unsigned NOT NULL,
-  `checked` tinyint(4) NOT NULL DEFAULT '0',
+  `checked` tinyint(1) NOT NULL DEFAULT '0',
   `worked` tinyint(1) NOT NULL DEFAULT '0',
   `checkdate` datetime NOT NULL DEFAULT '1980-01-01 00:00:00',
-  `fails` tinyint(4) NOT NULL DEFAULT '0',
+  `fails` tinyint(1) NOT NULL DEFAULT '0',
   `type` enum('HTTP_PROXY','SOCKS4_PROXY','SOCKS5_PROXY','DEAD_PROXY') NOT NULL DEFAULT 'DEAD_PROXY',
   `in_progress` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
   UNIQUE KEY `proxy` (`host`,`port`),
   KEY `sort` (`checked`,`checkdate`),
   KEY `type` (`type`)
-) ENGINE=MyISAM AUTO_INCREMENT=60622 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
