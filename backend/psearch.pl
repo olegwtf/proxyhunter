201c8177fcb623935d4ca34ba2696b0467047cf7
oleg <oleg@b7df0aa8-d4e2-4c31-b7a9-8b2298e1b5a8>
2010-12-12 07:19:28 +0000

    [FIX] search with different queries for each search engine
    git-svn-id: http://xp-dev.com/svn/proxyhunter@11 b7df0aa8-d4e2-4c31-b7a9-8b2298e1b5a8

--- backend/psearch.pl
+++ backend/psearch.pl
@@ -4,6 +4,7 @@ use strict;
 use lib('lib');
 use Socket;
 use DBI;
+use List::Util 'shuffle';
 use Net::Proxy::Search::Plugins;
 use Config::File 'read_config_file';
 
@@ -16,32 +17,37 @@ $SIG{INT} = $SIG{TERM} = sub { exit };
 
 my $plugins = Net::Proxy::Search::Plugins->new();
 $plugins->load();
-$plugins->init();
 
-my %plugins = %{$plugins};
 my $ip_regexp = qr/^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/;
+my @queries = values %{$cfg->{search_query}};
 my ($proxy, @proxylist);
 my ($host, $port, @hostent);
 
-while(%plugins) {
-	foreach my $module (keys %plugins) {
-		@proxylist = $plugins{$module}->next;
-		
-		foreach $proxy (@proxylist) {
-			($host, $port) = split /:/, $proxy;
+while(1) {
+	@queries = shuffle @queries;
+	$plugins->init(@queries);
+	my %plugins = %{$plugins};
+	
+	while(%plugins) {
+		foreach my $module (keys %plugins) {
+			@proxylist = $plugins{$module}->next;
 			
-			if($host !~ $ip_regexp) {
-				# hostname to ip for more efficient work with database
-				@hostent = gethostbyname($host)
-					or next;
-				$host = join('.', unpack('C4', $hostent[4]));
+			foreach $proxy (@proxylist) {
+				($host, $port) = split /:/, $proxy;
+				
+				if($host !~ $ip_regexp) {
+					# hostname to ip for more efficient work with database
+					@hostent = gethostbyname($host)
+						or next;
+					$host = join('.', unpack('C4', $hostent[4]));
+				}
+				
+				$sth->execute($host, $port);
 			}
 			
-			$sth->execute($host, $port);
+			delete $plugins{$module}
+				if $plugins{$module}->empty;
 		}
-		
-		delete $plugins{$module}
-			if $plugins{$module}->empty;
 	}
 }
 
